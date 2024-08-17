c8f5cd2364878e0cc39ed546c9f242795adb3969
jmLee4 <lijianming2018@email.szu.edu.cn>
2024-08-17 17:38:17 +0800

    Package LRU as cache and group, add RWMutex

---
+++ cache.go
@@ -0,0 +1,37 @@
+package go_cache
+
+import (
+	"github.com/jmLee4/go-cache/lru"
+	"sync"
+)
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.lru == nil {
+		// Lazy Initialization
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.lru == nil {
+		return
+	}
+
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+	return
+}
