b60d7f9dffb4203e12c086f3a464f650e76b26ba
jmLee4 <lijianming2018@email.szu.edu.cn>
2024-08-21 19:51:16 +0800

    Restore main.go and add Content-Disposition

--- main.go
+++ main.go
@@ -8,69 +8,77 @@ import (
 	"net/http"
 )
 
-var db = map[string]string{
+const (
+	APIServerRoute   = "/api"
+	APIServerKey     = "key"
+	DefaultGroupName = "scores"
+)
+
+var slowDB = map[string]string{
 	"Tom":  "630",
 	"Jack": "589",
 	"Sam":  "567",
 }
 
 func createGroup() *gocache.Group {
-	return gocache.NewGroup("scores", 1<<10, gocache.GetterFunc(
+	return gocache.NewGroup(DefaultGroupName, 5<<10, gocache.GetterFunc(
 		func(key string) ([]byte, error) {
-			log.Println("[SlowDB] search key", key)
-			if v, ok := db[key]; ok {
+			log.Println("[SlowDB] Search key", key)
+			if v, ok := slowDB[key]; ok {
 				return []byte(v), nil
 			}
 			return nil, fmt.Errorf("%s not exist", key)
 		}))
 }
 
-func startCacheServer(addr string, addrs []string, gee *gocache.Group) {
-	peers := gocache.NewHTTPPool(addr)
-	peers.Set(addrs...)
-	gee.RegisterPeers(peers)
-	log.Println("gocache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr[7:], peers))
+func startCacheServer(cacheServerAddr string, allCacheServerAddr []string, group *gocache.Group) {
+	cacheServer := gocache.NewHTTPPool(cacheServerAddr)
+	cacheServer.Set(allCacheServerAddr...)
+	group.RegisterPeers(cacheServer)
+	log.Println("GoCacheServer is running at", cacheServerAddr)
+	log.Fatal(http.ListenAndServe(cacheServerAddr[7:], cacheServer))
 }
 
-func startAPIServer(apiAddr string, gee *gocache.Group) {
-	http.Handle("/api", http.HandlerFunc(
+func startAPIServer(apiAddr string, group *gocache.Group) {
+	http.Handle(APIServerRoute, http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			key := r.URL.Query().Get("key")
-			view, err := gee.Get(key)
+			key := r.URL.Query().Get(APIServerKey)
+			view, err := group.Get(key)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
+			// TODO: Change file name
+			w.Header().Set("Content-Disposition", "attachment; filename=\"temp.txt\"")
 			w.Header().Set("Content-Type", "application/octet-stream")
-			w.Write(view.ByteSlice())
+			_, _ = w.Write(view.ByteSlice())
 		}))
-	log.Println("frontend server is running at", apiAddr)
+	log.Println("APIServer is running at", apiAddr)
 	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
 }
 
 func main() {
-	var port int
-	var api bool
-	flag.IntVar(&port, "port", 8001, "GoCache server port")
-	flag.BoolVar(&api, "api", false, "Start a api server?")
+	var cacheServerPort int
+	var startAPIServerFlag bool
+	flag.IntVar(&cacheServerPort, "cacheServerPort", 8001, "GoCache server port")
+	flag.BoolVar(&startAPIServerFlag, "startAPIServerFlag", false, "Start a APIServer?")
 	flag.Parse()
 
 	apiAddr := "http://localhost:9999"
-	addrMap := map[int]string{
+	port2Addr := map[int]string{
 		8001: "http://localhost:8001",
 		8002: "http://localhost:8002",
 		8003: "http://localhost:8003",
 	}
 
-	var addrs []string
-	for _, v := range addrMap {
-		addrs = append(addrs, v)
+	var allCacheServerAddr []string
+	for _, addr := range port2Addr {
+		allCacheServerAddr = append(allCacheServerAddr, addr)
 	}
 
-	gee := createGroup()
-	if api {
-		go startAPIServer(apiAddr, gee)
+	group := createGroup()
+	if startAPIServerFlag {
+		go startAPIServer(apiAddr, group)
 	}
-	startCacheServer(addrMap[port], addrs, gee)
+	startCacheServer(port2Addr[cacheServerPort], allCacheServerAddr, group)
 }
